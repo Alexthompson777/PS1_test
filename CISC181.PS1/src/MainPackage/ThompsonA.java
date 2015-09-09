bbffbacfe30428c02b67fb245ace0a3b136ff627
Alex Thompson <amthomps@udel.edu>
2015-09-08 23:33:53 -0400

    Added ThompsonA

---
+++ CISC181.PS1/src/MainPackage/ThompsonA.java
@@ -0,0 +1,14 @@
+package MainPackage;
+
+public class ThompsonA {
+	/*
+	 * My github name is Alexthompson777
+	 */
+	
+	
+	public static void HelloWorld(){
+		
+		System.out.println("Hello World");
+	}
+
+}
